fc0551c6aab4b0f3dcb75a06217ca1d0351e3c40
I-Strohmeyer <50380835+I-Strohmeyer@users.noreply.github.com>
2023-05-26 00:53:53 +0200

    added fake backend

--- src/App.js
+++ src/App.js
@@ -1,22 +1,78 @@
+import { useState } from "react";
 import "./App.css";
 
-import { Button, Heading, Text } from "@chakra-ui/react";
+import { Button, Text, Spinner, Box, Center } from "@chakra-ui/react";
+import { ReactComponent as YourSVG } from "./assets/logfig.svg";
 
 function App() {
+	const [loading, setLoading] = useState(false);
+	const [randomData, setRandomData] = useState({ text: "", emoji: "" });
+	//const [refresh, setRefresh] = useState(false);
+
+	const handleClick = () => {
+		setLoading(true);
+		setRandomData({ text: "", emoji: "" });
+
+		//Fake an API call with loading spinner
+		setTimeout(() => {
+			const data = [
+				{ text: "At least one person detected!", emoji: "👀" },
+				{ text: "Nothing detected", emoji: "🙈" },
+			];
+			const randomIndex = Math.floor(Math.random() * data.length);
+			setRandomData(data[randomIndex]);
+			setLoading(false);
+		}, 2000);
+	};
+
 	return (
 		<div className="App">
-			<Heading color="white" mb={8}>
-				Audit
-				<Text as="span" color="brand.800">
-					Guidar
-				</Text>
-			</Heading>
-			<Text color="white" mb={4}>
-				Checks if anyone is in the office.
+			<Box maxW="400px" h="200px">
+				<YourSVG
+					style={{ maxWidth: "400px", maxHeight: "80px" }}
+					alt="logo 2"
+				/>
+			</Box>
+			<Text color="white" mb={4} fontSize="24px">
+				Is someone in the DPS office?
 			</Text>
-			<Button bg="brand.900" _hover={{ backgroundColor: "#E78417" }}>
-				Get office status
-			</Button>
+			<Box
+				maxW="md"
+				bg="black"
+				boxShadow="rgba(255, 255, 255, 0.16) 0px 2px 20px"
+				p={16}
+				display="flex"
+				flexDirection="column"
+				alignItems="center"
+			>
+				<Button
+					bg="brand.900"
+					mb={12}
+					_hover={{ backgroundColor: "#E78417" }}
+					onClick={handleClick}
+					isLoading={loading}
+				>
+					Get status
+					{loading ? <Spinner size="sm" /> : null}
+				</Button>
+				<Text color="white">
+					{randomData.text && (
+						<>
+							<Center>
+								<Text
+									fontSize="32px"
+									role="img"
+									aria-label="Emoji"
+									mb={2}
+								>
+									{randomData.emoji}
+								</Text>
+							</Center>
+							<Text as="span">{randomData.text}</Text>
+						</>
+					)}
+				</Text>
+			</Box>
 		</div>
 	);
 }
